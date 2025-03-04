1cf79e941b63004bd168af7c806c8c0a0e51388a
Noah-Posin <nposin26@students.polytechnic.org>
2025-03-04 09:11:41 -0800

    finished road, still need to do move

--- car.java
+++ car.java
@@ -49,6 +49,9 @@ public class car {
             location--;
         }
     }
+    public int getLocation(){
+        return location;
+    }
     }
 
 
--- road.java
+++ road.java
@@ -1,22 +1,58 @@
 import java.util.ArrayList;
 
 public class road {
-private ArrayList<station> stations;
+private station[] stations;
 private ArrayList<car> cars;
 
 public road(){
 
     cars=new ArrayList<car>();
-    stations = new ArrayList<station>();
-}
-public void moveAll(){
-    for(car a : cars){
-        a.move();
+    stations = new station[32];
+    for(int i = 1;i<stations.length; i++){
+        stations[i]= new station();
     }
+
+}
+public void buildStations(int numppl){
+for( int i = 1;i<numppl; i++){
+    int start = (int)(Math.random()*numppl);
+    int stop = (int)(Math.random()*numppl);
+    stations[start].addPerson(new person(stop,start));
+}
+}
+public void buildCars(int numcars){
+for( int i = 1;i<numcars; i++){
+    int start = (int)(Math.random()*numcars);
+    int stop = (int)(Math.random()*numcars);
+    cars.add(new car(stop,start));
+
+}
+}
+
+public void moveCar(){
+    for(car c : cars){
+        c.unload();
+        int a =car.getLocation
+    }
+    
+
 }
 
 
+public String toString(){
 
+    String s ="";
+    for(station st : stations){
+        s += st.toString();
+    }
+    s+="Cars:\n";
+    for( car c : cars){
+        s+= c.toString();
+        s+="\n";
+    }
+    return s;
 }
+}
+
 
 
